afbd4c518c6b13a3da3e17cdfea953df5df598e1
Matheus Rizzieri <Mithio@live.com>
2020-10-26 23:41:30 -0300

    commit

---
+++ Observer_Method/src/BalancaDieta.java
@@ -0,0 +1,11 @@
+
+public class BalancaDieta implements Observer {
+       @Override
+       public void update(double novoPeso) {
+        if (peso >= 100) {
+            System.out.println("Você ultrapassou o peso limite no sistema!");
+        }
+       }
+}
+
+
